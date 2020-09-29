0cf0b0cde232166282c7488e991654e749867dbb
Igor Alexandre <iaxcsouza@gmail.com>
2020-09-29 16:35:24 -0400

    Corrigindo URL do http-common-js

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://grades-backend-app.herokuapp.com/',
+  baseURL: 'https://grades-backend-app.herokuapp.com/grade',
   headers: {
     'Content-type': 'application/json',
   },
